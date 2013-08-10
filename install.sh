e6b400fc91539866858aadbac9a2a9f2167fdde8
Bockra <bockra@gmail.com>
2013-08-10 23:01:05 +0400

    improved and moved instal.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function add_arch()
+{
+archive=$(whiptail --inputbox "Enter path to file/folder you want to backup \nLater you can edit archive.lst file line-by-line \nfor your backup list\nex. /home/user/folder or /var/log/main.log" 10 60 --title "$progname" 3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ] && [ ! -z "$archive" ]; then
+    echo $archive >> ./files/archive.lst
+else
+    message "You have to enter at least one file or dir to backup"
+    add_arch
+fi
+}
+
+function yesno()
+{
+whiptail --title "$progname" --yesno "Do you want to add one more item to backup?" --defaultno 10 60 
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+
+    add_arch
+    yesno
+else
+    echo "User selected No."
+fi
+}
+
+function message()
+{
+whiptail  --msgbox "$1" 8 60 --title "$progname"
+}
+
+progname="Aobackup"
+
+add_arch 
+yesno
+
+
